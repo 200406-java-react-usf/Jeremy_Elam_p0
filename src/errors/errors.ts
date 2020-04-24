8c2464d88e119c2ef49f8e9fc31d41cbd53d8ac1
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-23 18:31:59 -0700

    added packages

--- src/errors/errors.ts
+++ src/errors/errors.ts
@@ -1,48 +1,48 @@
 class ApplicationError{
-    message : string;
-    reason: string;
+	message : string;
+	reason: string;
 
-    constructor(reason?: string){
-        this.message = 'An unexpected error has occurred.';
-        reason ? (this.reason = reason) : this.reason = 'Unknown reason';
-    }
+	constructor(reason?: string){
+		this.message = 'An unexpected error has occurred.';
+		reason ? (this.reason = reason) : this.reason = 'Unknown reason';
+	}
 
-    setMessage(message:string){
-        this.message = message;
-    }
+	setMessage(message:string){
+		this.message = message;
+	}
 }
 
 class DataNotFoundError extends ApplicationError{
-    constructor(reason?: string){
-        super(reason);
-        super.setMessage('Error: No data was found')
-    }
+	constructor(reason?: string){
+		super(reason);
+		super.setMessage('Error: No data was found');
+	}
 }
 
 class InvalidRequestError extends ApplicationError{
-    constructor(reason?: string){
-        super(reason);
-        super.setMessage('Error: Invalid Request')
-    }
+	constructor(reason?: string){
+		super(reason);
+		super.setMessage('Error: Invalid Request');
+	}
 }
 
 class AuthenticationError extends ApplicationError{
-    constructor(reason?: string){
-        super(reason);
-        super.setMessage('Error: Authentication Failed')
-    }
+	constructor(reason?: string){
+		super(reason);
+		super.setMessage('Error: Authentication Failed');
+	}
 }
 
 class DataNotStoredError extends ApplicationError{
-    constructor(reason?: string){
-        super(reason);
-        super.setMessage('Error: The data has not been stored')
-    }
+	constructor(reason?: string){
+		super(reason);
+		super.setMessage('Error: The data has not been stored');
+	}
 }
 
 export{
-    DataNotFoundError,
-    DataNotStoredError,
-    AuthenticationError,
-    InvalidRequestError
-}
+	DataNotFoundError,
+	DataNotStoredError,
+	AuthenticationError,
+	InvalidRequestError
+};
