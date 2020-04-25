bd317bde0e1734b8b98ee72114df30417cb2d684
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-25 11:26:23 -0700

    added mock dataset

--- src/data/card-db.ts
+++ src/data/card-db.ts
@@ -1,1 +1,9 @@
+import {Cards} from '../models/cards';
 
+export default [ 
+	new Cards ('Domri something something', 'IDK', 'Mythic', 20.00),
+	new Cards ('Elspeth something something', 'IDK', 'Mythic', 8.00),
+	new Cards ('Teferi something something', 'IDK', 'Mythic', 16.88),
+	new Cards ('Nissa something something', 'IDK', 'Mythic', 8.46),
+	new Cards ('Vivien something something', 'IDK', 'Mythic', 3.58),
+];
