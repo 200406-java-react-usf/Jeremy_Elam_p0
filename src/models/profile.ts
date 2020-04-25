9104835059d5f5e665ad24ebe3ac9228f7eeb125
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-25 11:17:27 -0700

    changed color from a string to object

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -2,11 +2,11 @@ export class UserProfile {
 	user_un: string;
 	user_id: number;
 	fav_archetypes: string;
-	fav_colors: string;
+	fav_colors: object;
 	fav_card: string;
 	location: string;
 
-	constructor (un:string, id:number, archetype: string, color: string, card: string, location: string){
+	constructor (un:string, id:number, archetype: string, color: object, card: string, location: string){
 		this.user_un = un;
 		this.user_id =id;
 		this.fav_archetypes = archetype;
