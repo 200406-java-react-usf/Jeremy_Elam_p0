81dc1ef7b3c831431274431fcbea151ae88f3ade
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-25 17:12:06 -0700

    removed the s from repos folder

--- src/repos/crud-repo.ts
+++
@@ -1,7 +0,0 @@
-export interface CrudRepository<T>{
-    getAll(): Promise<T[]>;
-    getById(id: number | string): Promise<T>;
-    save(newObj: T): Promise<T>;
-    update(updateObj: T):Promise<boolean>;
-    deleteById(id: number | string): Promise<boolean>;
-}
