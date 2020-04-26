f9c179f8522e257b796ca44091e0eaa72e58d442
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-26 12:11:28 -0700

    testing to make sure logic is working

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import { UserInfo } from './models/user';
 
 let invalidMockUser = new UserInfo(99999, 'update','update','update','update', new Date());
 (async () =>{
-	let testing = await UserRepository.getInstance().update(invalidMockUser);
+	try{
+		let testing = await UserRepository.getInstance().update(invalidMockUser);
 	console.log(testing);
+	}catch(e){
+		console.log(e);
+		
+	}
+	
 	
 	console.log(await UserRepository.getInstance().getAll());
 	
