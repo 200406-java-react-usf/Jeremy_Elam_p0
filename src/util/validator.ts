446ffe6fded273cd69459d8ce975f63efcca0fbf
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-26 14:56:36 -0700

    i don't know why this needs to be commited no change for diff

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -14,8 +14,17 @@ const isValidObject = (obj: Object, ...nullableProps: string[]) => {
 	});
 };
 
+const isCardValidObject = (obj: Object, ...nullableProps: string[]) => {
+	return obj && Object.keys(obj).every(key => {
+		if (key == 'card_name') return isValidStrings(obj['card_name']);
+		if (nullableProps.includes(key)) return true;
+		return obj[key];
+	});
+};
+
 export default{
 	isValidId,
 	isValidStrings,
-	isValidObject
+	isValidObject,
+	isCardValidObject
 };
