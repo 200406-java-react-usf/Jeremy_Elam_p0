adcf692833c7b00967d7dddef9a62710f84b3352
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-26 17:56:15 -0700

    testing save method to make sure it works

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
 import { UserRepository } from './repo/user-repo';
 import {CardRepository} from './repo/card-repo';
 import { UserInfo } from './models/user';
+import { Cards } from './models/cards'
 
 
 let invalidMockUser = new UserInfo(99999, 'update','update','update','update', new Date());
-(async () =>{
-	try{
-		let testing = await UserRepository.getInstance().update(invalidMockUser);
-	console.log(testing);
-	}catch(e){
-		console.log(e);
+// (async () =>{
+// 	try{
+// 		let testing = await UserRepository.getInstance().update(invalidMockUser);
+// 	console.log(testing);
+// 	}catch(e){
+// 		console.log(e);
 		
-	}
+// 	}
 	
 	
-	console.log(await UserRepository.getInstance().getAll());
+// 	console.log(await UserRepository.getInstance().getAll());
 	
-})();
+// })();
 
 (async()=>{
+	let invalidMockUser = new Cards("Elspeth something something", "IDK", "Primordial",400.00);
+
 	try{
-		let testing = await CardRepository.getInstance().getById('Domri something something')
+		let testing = await CardRepository.getInstance().save(invalidMockUser)
 		console.log(testing);
 	}catch(e){
 		console.log(e);
