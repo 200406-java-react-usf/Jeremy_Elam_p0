11856dd03d7e73f5ec4a5a8a13656cfc2ca27eae
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-26 18:28:04 -0700

    testing to make sure the card update method is working

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,25 @@ let invalidMockUser = new UserInfo(99999, 'update','update','update','update', n
 	
 // })();
 
+// (async()=>{
+// 	let invalidMockUser = new Cards("Elspeth something something", "IDK", "Primordial",400.00);
+
+// 	try{
+// 		let testing = await CardRepository.getInstance().save(invalidMockUser)
+// 		console.log(testing);
+// 	}catch(e){
+// 		console.log(e);
+// 	}
+// })();
+
 (async()=>{
 	let invalidMockUser = new Cards("Elspeth something something", "IDK", "Primordial",400.00);
 
 	try{
-		let testing = await CardRepository.getInstance().save(invalidMockUser)
+		let testing = await CardRepository.getInstance().update(invalidMockUser);
 		console.log(testing);
 	}catch(e){
 		console.log(e);
 	}
-})();
+})()
 
