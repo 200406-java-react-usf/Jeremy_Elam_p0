ebf1a17e7e48208c79eccfcd7536e1dced20a012
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-29 16:29:14 -0700

    removed date added roles

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,14 +4,14 @@ export class UserInfo {
 	user_ln: string;
 	user_email: string;
 	user_pw: string;
-	user_dob: Date;
+	role: string;
 
-	constructor (us_id:number, fn:string, ln:string, email:string, pw:string, dob:Date){
+	constructor (us_id:number, fn:string, ln:string, email:string, pw:string, role: string){
 		this.id = us_id;
 		this.user_fn = fn;
 		this.user_ln = ln;
 		this.user_email = email;
 		this.user_pw = pw;
-		this.user_dob = dob;
+		this.role = role
 	}
 }
