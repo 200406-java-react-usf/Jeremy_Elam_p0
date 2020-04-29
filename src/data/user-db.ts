db42eb787d3bfb6daecafbba0b8065ae901ae43f
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-29 16:28:11 -0700

    removed date from data and added Role

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -3,9 +3,9 @@ import { UserInfo } from '../models/user';
 let id = 1;
 
 export default[
-	new UserInfo(id++, 'Jeremy', 'Elam', 'jeremyelam@gmail.com', 'password', new Date('09/17/1994')),
-	new UserInfo(id++, 'Kenan', 'Hilman', 'flameking0127@gmail.com', 'password', new Date('01/27/2000')),
-	new UserInfo(id++, 'Jalen', 'Hilman', 'jalenhilman@gmail.com', 'password', new Date('02/14/2005')),
-	new UserInfo(id++, 'Ameline', 'Chua', 'amelinechua@gmail.com', 'password', new Date('09/17/1994')),
-	new UserInfo(id++, 'Salt', 'Elam', 'saltelam@gmail.com', 'password', new Date('10/15/2018'))
+	new UserInfo(id++, 'Jeremy', 'Elam', 'jeremyelam@gmail.com', 'password', "Admin"),
+	new UserInfo(id++, 'Kenan', 'Hilman', 'flameking0127@gmail.com', 'password', "User"),
+	new UserInfo(id++, 'Jalen', 'Hilman', 'jalenhilman@gmail.com', 'password', "User"),
+	new UserInfo(id++, 'Ameline', 'Chua', 'amelinechua@gmail.com', 'password', "User"),
+	new UserInfo(id++, 'Salt', 'Elam', 'saltelam@gmail.com', 'password', "Cat")
 ];
