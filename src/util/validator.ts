42a7f9c89d3866f46b9cc2b33f8b0141d7776644
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-28 22:17:34 -0700

    added new validators

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -1,12 +1,12 @@
-const isValidId = (id: number): boolean =>{
+export const isValidId = (id: number): boolean =>{
 	return(id && typeof id === 'number' && Number.isInteger(id) && id > 0);
 };
 
-const isValidStrings = (...strs: string[]): boolean => {
+export const isValidStrings = (...strs: string[]): boolean => {
 	return (strs.filter(str => !str || typeof str !== 'string').length == 0);
 };
 
-const isValidObject = (obj: Object, ...nullableProps: string[]) => {
+export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
 	return obj && Object.keys(obj).every(key => {
 		if (key == 'id') return isValidId(obj['id']);
 		if (nullableProps.includes(key)) return true;
@@ -14,7 +14,23 @@ const isValidObject = (obj: Object, ...nullableProps: string[]) => {
 	});
 };
 
-const isCardValidObject = (obj: Object, ...nullableProps: string[]) => {
+export const isPropertyOf = (prop: string, type: any) =>{
+	if(!prop || !type){
+		return false;
+	}
+	let typeCreator = <T>(Type: (new()=>T)): T =>{
+		return new Type();
+	}
+	let tempInstance; 
+	try{
+		tempInstance = typeCreator(type);
+	}catch{
+		return false;
+	}
+	return Object.keys(tempInstance).includes(prop);
+}
+
+export const isCardValidObject = (obj: Object, ...nullableProps: string[]) => {
 	return obj && Object.keys(obj).every(key => {
 		if(key == 'card_price') return isValidPrice(obj['card_price']);
 		if (key == 'card_name') return isValidStrings(obj['card_name']);
@@ -22,14 +38,22 @@ const isCardValidObject = (obj: Object, ...nullableProps: string[]) => {
 		return obj[key];
 	});
 };
-const isValidPrice = (price: number): boolean =>{
+export const isValidPrice = (price: number): boolean =>{
 	return(price && typeof price === 'number' && price > 0);
 };
+
+export function isEmptyObject<T>(obj: T){
+	return obj && Object.keys(obj).length === 0;
+}
+
+
 
 export default{
 	isValidId,
 	isValidStrings,
 	isValidObject,
 	isCardValidObject,
-	isValidPrice
+	isValidPrice,
+	isEmptyObject,
+	isPropertyOf
 };
