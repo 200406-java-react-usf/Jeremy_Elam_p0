7be12c2c5a88986a5d601301d1a89b47bcbb7f0a
jeremy2975 <=Jeremy2975@yahoo.com>
2020-04-28 22:14:08 -0700

    added principal method for future use

---
+++ src/dtos/principal.ts
@@ -0,0 +1,11 @@
+export class principal {
+	id: number;
+	user_email: string;
+	role: string;
+
+	constructor(id:number, email: string, role: string){
+		this.id = id,
+		this.user_email = email,
+		this. role = role
+	}
+}
