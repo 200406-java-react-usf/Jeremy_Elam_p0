a7aa52c7edb7eadd9fa5ad86eb7710f127a88450
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-01 22:58:01 -0700

    added schemas to project

---
+++ src/util/schemas.ts
@@ -0,0 +1,16 @@
+export interface UserSchema{
+	id: number, 
+	first_name: string;
+	last_name: string;
+	email: string;
+	password: string;
+	role_name: string
+}
+
+export interface ProfileSchema{
+
+};
+
+export interface CardSchema{
+
+};
