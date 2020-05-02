08c823b9146396c073c9f410ceb19eddd97819a1
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-01 22:57:33 -0700

    added mapper to project

---
+++ src/util/result-set-mapper.ts
@@ -0,0 +1,16 @@
+import {UserSchema} from './schemas';
+import {UserInfo} from '../models/user'
+
+export function mapUserResultSet(resultSet: UserSchema): UserInfo{
+	if(!resultSet){
+		return {} as UserInfo;
+	}
+	return new UserInfo(
+		resultSet.id,
+		resultSet.first_name,
+		resultSet.last_name,
+		resultSet.email,
+		resultSet.password,
+		resultSet.role_name
+	);
+}
