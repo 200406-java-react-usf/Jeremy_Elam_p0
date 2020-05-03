a196ed961a0d21e687c2a18dcda76bd347c05123
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-02 22:48:17 -0700

    added id numbers to gimpped database so i can run code

--- src/data/card-db.ts
+++ src/data/card-db.ts
@@ -1,9 +1,9 @@
 import {Cards} from '../models/cards';
 
 export default [ 
-	new Cards ('Domri something something', 'IDK', 'Mythic', 20.00),
-	new Cards ('Elspeth something something', 'IDK', 'Mythic', 8.00),
-	new Cards ('Teferi something something', 'IDK', 'Mythic', 16.88),
-	new Cards ('Nissa something something', 'IDK', 'Mythic', 8.46),
-	new Cards ('Vivien something something', 'IDK', 'Mythic', 3.58),
+	new Cards (1,'Domri something something', 'IDK', 'Mythic', 20.00),
+	new Cards (2,'Elspeth something something', 'IDK', 'Mythic', 8.00),
+	new Cards (3,'Teferi something something', 'IDK', 'Mythic', 16.88),
+	new Cards (4,'Nissa something something', 'IDK', 'Mythic', 8.46),
+	new Cards (5,'Vivien something something', 'IDK', 'Mythic', 3.58),
 ];
