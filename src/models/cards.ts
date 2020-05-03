6b193424e86cb63e8e5082a0ca77a628571519ba
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-02 22:48:58 -0700

    added numberical id to card model to match database

--- src/models/cards.ts
+++ src/models/cards.ts
@@ -1,10 +1,12 @@
 export class Cards{
+	id: number;
 	card_name: string;
 	card_set: string;
 	card_rarity: string;
 	card_price: number;
 
-	constructor(card_name: string, card_set: string, card_rarity: string, card_price: number){
+	constructor(id:number,card_name: string, card_set: string, card_rarity: string, card_price: number){
+		this.id = id;
 		this.card_name = card_name;
 		this.card_set = card_set;
 		this.card_rarity = card_rarity;
