17224ad6dd55c9c954c79c521d72d2e4e3cbb5c0
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-03 14:10:28 -0700

    no real change to crud-repo. tested something but it didn't work

--- src/repo/crud-repo.ts
+++ src/repo/crud-repo.ts
@@ -1,7 +1,7 @@
 export interface CrudRepository<T>{
     getAll(): Promise<T[]>;
-    getById(id: number | string): Promise<T>;
+    getById(id: number): Promise<T>;
     save(newObj: T): Promise<T>;
     update(updateObj: T):Promise<boolean>;
-    deleteById(id: string | number): Promise<boolean>;
+    deleteById(id:number): Promise<boolean>;
 }
