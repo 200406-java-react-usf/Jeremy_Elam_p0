41822e8870619e5bfa8fa49c14784fba25a27aa0
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-02 22:51:53 -0700

    update card-router to connect with sql database

--- src/routers/card-router.ts
+++ src/routers/card-router.ts
@@ -11,19 +11,36 @@ const CardService = AppConfig.cardService;
 
 CardRouter.get('', async(req,resp)=>{
 	try{
-		let payload = await CardService.getAllCards();
-		return resp.status(200).json(payload);
+		let reqURL = url.parse(req.url,true);
+		if(!isEmptyObject<ParsedUrlQuery>(reqURL.query)){
+			let payload = await CardService.getCardByUniqueKey({...reqURL.query});
+			resp.status(200).json(payload);
+		}else{
+			let payload = await CardService.getAllCards();	
+			resp.status(200).json(payload);
+		}
 	}catch(e){
 		return resp.status(404).json(e);
 	}
 });
 
 CardRouter.get('/:id', async(req, resp) =>{
-	const id = req.params.id;
+	const id = +req.params.id;
 	try{
 		let payload = await CardService.getCardById(id);
 		return resp.status(200).json(payload);
 	}catch(e){
 		return resp.status(404).json(e);
 	}
+});
+
+CardRouter.post('', async(req, resp) =>{
+	console.log('POST REQUEST RECEIVED AT /cards');
+	console.log(req.body);
+	try{
+		let payload = await CardService.addNewCard(req.body);
+		return resp.status(201).json(payload);
+	}catch(e){
+		return resp.status(e.statusCode).json(e);
+	}	
 });
