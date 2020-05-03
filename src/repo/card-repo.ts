db85a0039b2b985d0f22ded53c096223b1726b7a
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-02 22:50:40 -0700

    refactored card-repo to connect with database, has getcardbyid, getall, getcardbyunique, and a save method

--- src/repo/card-repo.ts
+++ src/repo/card-repo.ts
@@ -7,45 +7,89 @@ import {
 	AuthenticationError, 
 	ResourceNotFoundError, 
 	ResourcePersistenceError,
-	NotImplementedError 
+	NotImplementedError, 
+	InternalServerError
 } from '../errors/errors';
-import userDb from '../data/user-db';
+import {PoolClient} from 'pg';
+import {connectionPool} from '..';
+import {mapCardResultSet, mapUserResultSet} from '../util/result-set-mapper';
 
 export class CardRepository implements CrudRepository<Cards>{
 	
-	getAll(): Promise<Cards[]>{
-		return new Promise<Cards[]>((resolve,reject)=>{
-			setTimeout(()=> {
-				let cards: Cards[] = data;
-				resolve(cards);
-			});
-		});
+	baseQuery = `select * from full_card_info`
+	async getAll(): Promise<Cards[]>{
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			let sql = `${this.baseQuery}`;
+			let rs =await client.query(sql);
+			console.log(rs);
+			
+			return rs.rows.map(mapCardResultSet);
+		}catch(e){
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
 	}
-	getById(name: string): Promise<Cards>{
-		return new Promise<Cards>((resolve,reject)=>{
-			setTimeout(()=>{
-				const card = {...data.find(card => card.card_name === name)};
-				resolve(card);
-			},1000);
-		});
+	async getById(id: number): Promise<Cards>{
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			let sql = `${this.baseQuery} where id = ${id}`;
+			let rs = await client.query(sql);
+			return mapCardResultSet(rs.rows[0])
+		}catch(e){
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
 	}
-	save(newCard: Cards): Promise<Cards>{
-		return new Promise<Cards>((resolve, reject) =>{
-			if(!validator.isCardValidObject(newCard, 'card_name')){
-				reject(new BadRequestError('Invalid property values found in provided card.'));
-				return;
-			}
-			setTimeout(()=>{
-				let conflict = data.filter(card => card.card_name == newCard.card_name).pop();
-				if(conflict){
-					reject(new ResourcePersistenceError('The provided card name is already being used'));
-					return;
-				}
 
-				data.push(newCard);
-				resolve(newCard);
-			});
-		});
+	async getCardByUniqueKey(key: string, val: string):Promise<Cards>{
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			let sql = `${this.baseQuery} where ${key} = $1`;
+			console.log(val);
+			console.log(key);
+			
+			let rs = await client.query(sql, [val]);
+			console.log(rs);
+			
+			return mapCardResultSet(rs.rows[0]);
+		}catch (e){
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
+	}
+	async save(newCard: Cards): Promise<Cards>{
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			console.log("______________________________________________________");
+			
+			let rarityID = (await client.query('select id from card_rarities where card_rarity = $1', [newCard.card_rarity])).rows[0].id;
+			console.log("______________________________________________________");
+			
+			console.log(rarityID);
+			console.log("______________________________________________________");
+			
+			let setID = (await client.query('select id from card_sets where card_set = $1', [newCard.card_set])).rows[0].id;
+			console.log(setID);
+			console.log("______________________________________________________");
+
+			let sql = 'insert into card_info(card_name, card_set, card_rarity, card_price) values($1,$2,$3,$4) returning id';
+			let rs = await client.query(sql, [newCard.card_name, setID, rarityID, newCard.card_price]);
+			newCard.id = rs.rows[0].id;
+			return newCard;
+		}catch(e){
+			// console.log(e);
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
 	}
 	update(updateCard: Cards): Promise<boolean>{
 		return new Promise<boolean>((resolve, reject)=>{
