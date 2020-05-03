6bb905673228e3624f37e040814934e574771632
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-03 15:48:48 -0700

    removed gimpped on data for profiles

--- src/data/profile-db.ts
+++ src/data/profile-db.ts
@@ -1,9 +1,2 @@
 import {UserProfile} from '../models/profile';
 
-export default[
-	new UserProfile('lazyspell', 1, 'bant',['green','blue','white'], 'Domri','GamerKings' ),
-	new UserProfile('Thing 1', 2, 'Naya',['green', 'white','red'], 'Shock','GamerKings' ),
-	new UserProfile('Thing 2', 3, 'esper',['black','blue','white'], 'Elspeth','GamerKings' ),
-	new UserProfile('mother', 1, 'mono-red',['red'], 'Chandra','GamerKings' ),
-	new UserProfile('cat', 1, 'mono-black',['black'], 'Thoughtseize','GamerKings' )
-];
