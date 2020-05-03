1317a869c3224f38c13d8732c026eccf32ede7d5
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-03 14:09:30 -0700

    added updated and delete methods to card-repo

--- src/repo/card-repo.ts
+++ src/repo/card-repo.ts
@@ -3,11 +3,6 @@ import { Cards } from '../models/cards';
 import {CrudRepository} from './crud-repo';
 import validator from '../util/validator';
 import {  
-	BadRequestError, 
-	AuthenticationError, 
-	ResourceNotFoundError, 
-	ResourcePersistenceError,
-	NotImplementedError, 
 	InternalServerError
 } from '../errors/errors';
 import {PoolClient} from 'pg';
@@ -16,15 +11,13 @@ import {mapCardResultSet, mapUserResultSet} from '../util/result-set-mapper';
 
 export class CardRepository implements CrudRepository<Cards>{
 	
-	baseQuery = `select * from full_card_info`
+	baseQuery = 'select * from full_card_info'
 	async getAll(): Promise<Cards[]>{
 		let client: PoolClient;
 		try{
 			client = await connectionPool.connect();
 			let sql = `${this.baseQuery}`;
 			let rs =await client.query(sql);
-			console.log(rs);
-			
 			return rs.rows.map(mapCardResultSet);
 		}catch(e){
 			throw new InternalServerError();
@@ -38,7 +31,7 @@ export class CardRepository implements CrudRepository<Cards>{
 			client = await connectionPool.connect();
 			let sql = `${this.baseQuery} where id = ${id}`;
 			let rs = await client.query(sql);
-			return mapCardResultSet(rs.rows[0])
+			return mapCardResultSet(rs.rows[0]);
 		}catch(e){
 			throw new InternalServerError();
 		}finally{
@@ -51,12 +44,7 @@ export class CardRepository implements CrudRepository<Cards>{
 		try{
 			client = await connectionPool.connect();
 			let sql = `${this.baseQuery} where ${key} = $1`;
-			console.log(val);
-			console.log(key);
-			
 			let rs = await client.query(sql, [val]);
-			console.log(rs);
-			
 			return mapCardResultSet(rs.rows[0]);
 		}catch (e){
 			throw new InternalServerError();
@@ -68,18 +56,10 @@ export class CardRepository implements CrudRepository<Cards>{
 		let client: PoolClient;
 		try{
 			client = await connectionPool.connect();
-			console.log("______________________________________________________");
-			
+		
 			let rarityID = (await client.query('select id from card_rarities where card_rarity = $1', [newCard.card_rarity])).rows[0].id;
-			console.log("______________________________________________________");
-			
 			console.log(rarityID);
-			console.log("______________________________________________________");
-			
 			let setID = (await client.query('select id from card_sets where card_set = $1', [newCard.card_set])).rows[0].id;
-			console.log(setID);
-			console.log("______________________________________________________");
-
 			let sql = 'insert into card_info(card_name, card_set, card_rarity, card_price) values($1,$2,$3,$4) returning id';
 			let rs = await client.query(sql, [newCard.card_name, setID, rarityID, newCard.card_price]);
 			newCard.id = rs.rows[0].id;
@@ -91,30 +71,40 @@ export class CardRepository implements CrudRepository<Cards>{
 			client && client.release();
 		}
 	}
-	update(updateCard: Cards): Promise<boolean>{
-		return new Promise<boolean>((resolve, reject)=>{
-			if(!validator.isCardValidObject(updateCard)){
-				reject(new BadRequestError('Invalid card provided (invalid values found).'));
-				return;
-			}
-			setTimeout(()=>{
-				let persistedCard = data.find(card => card.card_name === updateCard.card_name);
-				if(!persistedCard){
-					reject(new ResourceNotFoundError('No user found with provided id.'));
-					return;
-				}
-				persistedCard = updateCard;
-				resolve(true);
-			});
-		});
+	async update(updateCard: Cards): Promise<boolean>{
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			
+			let cardRarity = (await client.query('select id from card_rarities where card_rarity = $1',[updateCard.card_rarity])).rows[0].id;
+			console.log(cardRarity);
+			
+			let cardSet = (await client.query('select id from card_sets where card_set = $1',[updateCard.card_set])).rows[0].id;
+			console.log(cardSet);
+			
+			let sql = 'update card_info set card_set = $2, card_rarity = $3, card_price = $4 where id =$1';
+			await client.query(sql, [updateCard.id, cardSet, cardRarity, updateCard.card_price]);
+			return true;
+		}catch(e){
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
 	}
-	deleteById(name:string): Promise<boolean>{
-		return new Promise<boolean>((resolve, rejects)=>{
-			if(!validator.isValidId){
-				rejects(new BadRequestError('Invalid Card Name was provided'));
-			}
-			rejects(new NotImplementedError());
-		});
+	async deleteById(id: number): Promise<boolean>{
+		console.log('3');
+		
+		let client: PoolClient;
+		try{
+			client = await connectionPool.connect();
+			let sql = 'delete from card_info where id = $1';
+			let rs = await client.query(sql, [id]);
+			return rs.rows[0];
+		}catch(e){
+			throw new InternalServerError();
+		}finally{
+			client && client.release();
+		}
 	}
 }
 	
