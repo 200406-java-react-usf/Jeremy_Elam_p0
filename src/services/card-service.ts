8eeca7f94c175c5e7208183a473024a03fd4211c
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-02 22:52:26 -0700

    refactored card-service to work with database

--- src/services/card-service.ts
+++ src/services/card-service.ts
@@ -9,38 +9,81 @@ import {
 	AuthenticationError 
 } from '../errors/errors';
 import cardDb from '../data/card-db';
+import { UserService } from './user-services';
 
 export class CardService{
 	constructor(private cardRepo: CardRepository){
 		this.cardRepo = cardRepo;
 	}
-	getAllCards(): Promise<Cards[]>{
-		return new Promise<Cards[]>(async(resolve, rejected)=>{
-			let cards: Cards[] = [];
-
-			let result = await this.cardRepo.getAll();
-
-			for (let card of result){
-				cards.push({...card});
-			}
-
-			if(cards.length === 0){
-				rejected(new ResourceNotFoundError());
-				return;
-			}
-			resolve(cards);
-		});
+	async getAllCards(): Promise<Cards[]>{
+		let cards = await this.cardRepo.getAll();
+		if(cards.length === 0){
+			throw new ResourceNotFoundError();
+		}
+		return cards
 	}
-	getCardById(id:string): Promise<Cards>{
-		return new Promise<Cards>(async(resolve, rejects)=>{
-			if(!isValidStrings(id)){
-				return rejects(new BadRequestError());
+	async getCardById(id:number): Promise<Cards>{
+		if(!isValidId(id)){
+			throw new BadRequestError();
+		}
+		let cards = {...await this.cardRepo.getById(id)};
+		if(isEmptyObject(cards)){
+			throw new ResourceNotFoundError();
+		}
+		return cards;
+	}
+	async getCardByUniqueKey(queryObj: any): Promise<Cards>{
+		try{
+			let queryKeys = Object.keys(queryObj);
+			console.log('1');
+			
+			if(!queryKeys.every(key => isPropertyOf(key, Cards))){
+				throw new BadRequestError();
 			}
-			let cards = {...await this.cardRepo.getById(id)};
-			if(isEmptyObject(cards)){
-				return rejects(new ResourceNotFoundError());
+			console.log('2');
+			
+			let key = queryKeys[0];
+			let val = queryObj[key];
+			if(key === 'id'){
+				return await this.getCardById(+val);
 			}
-			resolve(cards);
-		});
+			console.log('3');
+			
+			if(!isValidStrings(val)){
+				throw new BadRequestError();
+			}
+			let card = await this.cardRepo.getCardByUniqueKey(key, val);
+			if(isEmptyObject(card)){
+				throw new ResourceNotFoundError();
+			}
+			return card;
+		}catch (e){
+			throw e;
+		}
+	}
+	async addNewCard(newCard: Cards): Promise<Cards>{
+		try{
+			if(!isValidObject(newCard, 'id')){
+				throw new BadRequestError('Invalid property values fround in provided user.');
+			}
+			let cardNameAvailable = await this.isCardNameAvailable(newCard.card_name);
+			if(!cardNameAvailable){
+				throw new ResourcePersistenceError('The provided card name is already in use.')
+			}
+			const persistedCard = await this.cardRepo.save(newCard);
+			return persistedCard;
+		}catch(e){
+			throw e;
+		}
+	}
+	private async isCardNameAvailable(cardName: string): Promise<boolean>{
+		try{
+			await this.getCardByUniqueKey({'card_name':cardName});
+		}catch(e){
+			console.log('card name is available');
+			return true;
+		}
+		console.log('email is available ');
+		return false;
 	}
 }
