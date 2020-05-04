594caade512ab4a2c8e0711dd65d90ea8f04443b
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-04 14:48:44 -0700

    not too sure what was changed, but this database will be removed soon anyways

--- src/data/user-db.ts
+++ src/data/user-db.ts
@@ -7,5 +7,5 @@ export default[
 	new UserInfo(id++, 'Kenan', 'Hilman', 'flameking0127@gmail.com', 'password', 'User'),
 	new UserInfo(id++, 'Jalen', 'Hilman', 'jalenhilman@gmail.com', 'password', 'User'),
 	new UserInfo(id++, 'Ameline', 'Chua', 'amelinechua@gmail.com', 'password', 'User'),
-	new UserInfo(id++, 'Salt', 'Elam', 'saltelam@gmail.com', 'password', 'Cat')
+	new UserInfo(id++, 'Salt', 'Elam', 'saltelam@gmail.com', 'password', 'Admin')
 ];
