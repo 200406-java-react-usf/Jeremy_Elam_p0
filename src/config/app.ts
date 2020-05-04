c518a4e322e4c8798c386dd44aa3ba6d52b17dc9
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-03 23:40:19 -0700

    added manual singlton to profiles

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,6 +2,11 @@ import {UserRepository} from '../repo/user-repo';
 import{UserService} from '../services/user-services';
 import{CardRepository} from '../repo/card-repo';
 import{CardService} from '../services/card-service';
+import{ProfileRepository} from '../repo/profile-repo';
+import{ProfileService} from '../services/profile-service';
+
+const profileRepo = new ProfileRepository();
+const profileService = new ProfileService(profileRepo);
 
 const userRepo = new UserRepository();
 const userService = new UserService(userRepo);
@@ -12,6 +17,7 @@ const cardService = new CardService(cardRepo);
 
 export default{
 	userService,
-	cardService
+	cardService,
+	profileService
 };
 
