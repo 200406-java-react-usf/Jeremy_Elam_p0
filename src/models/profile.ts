7809a74cddcb2471664c91dbd6e09343eeec922b
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-03 17:37:49 -0700

    update models to match what was needed

--- src/models/profile.ts
+++ src/models/profile.ts
@@ -1,16 +1,18 @@
 export class UserProfile {
 	user_un: string;
-	id: number;
+	profile_id: number;
 	fav_archetypes: string;
-	fav_colors: object;
-	fav_card: string;
+	fav_colors: string;
+	card_set: string;
+	card_name: string;
 	user_info: number;
-	constructor (un:string, id:number, archetype: string, color: object, card: string, user_info: number){
+	constructor (un:string, id:number, archetype: string, color: string,set: string, card: string, user_info: number){
 		this.user_un = un;
-		this.id =id;
+		this.profile_id =id;
 		this.fav_archetypes = archetype;
 		this.fav_colors = color;
-		this.fav_card = card;
+		this.card_set = set;
+		this.card_name = card;
 		this.user_info = user_info;
 	}
 }
