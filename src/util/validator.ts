97bd8279da8ae1c1ff98509612661c1264ef35de
jeremy2975 <=Jeremy2975@yahoo.com>
2020-05-05 23:36:12 -0700

    added documentation for validators

--- src/util/validator.ts
+++ src/util/validator.ts
@@ -1,11 +1,24 @@
+/**
+ * Checks to see if the Number passed in is a valid ID Number
+ * @param id 
+ */
 export const isValidId = (id: number): boolean =>{
 	return !!(id && typeof id === 'number' && Number.isInteger(id) && id > 0);
 };
 
+/**
+ * Checks to see if the string passed in is an actual string
+ * @param strs 
+ */
 export const isValidStrings = (...strs: string[]): boolean => {
 	return (strs.filter(str => !str || typeof str !== 'string').length == 0);
 };
 
+/**
+ * checks to see if the object passed in is a real object not a fasly object
+ * @param obj 
+ * @param nullableProps 
+ */
 export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
 	return obj && Object.keys(obj).every(key => {
 		if (nullableProps.includes(key)) return true;
@@ -13,6 +26,11 @@ export const isValidObject = (obj: Object, ...nullableProps: string[]) => {
 	});
 };
 
+/**
+ * checks to see if the key value passed in is a property of the constructor you're checking
+ * @param prop 
+ * @param type 
+ */
 export const isPropertyOf = (prop: string, type: any) =>{
 	if(!prop || !type){
 		return false;
@@ -41,6 +59,10 @@ export const isPropertyOf = (prop: string, type: any) =>{
 // 	return(price && typeof price === 'number' && price > 0);
 // };
 
+/**
+ * checks to see if an object passed in is empty
+ * @param obj 
+ */
 export function isEmptyObject<T>(obj: T){
 	return obj && Object.keys(obj).length === 0;
 }
